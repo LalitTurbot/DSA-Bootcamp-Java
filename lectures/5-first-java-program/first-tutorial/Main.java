9ed9f67775aab5190b043147459ae930f73d12b8
LalitTurbot <lalit@turbot.com>
2021-09-23 13:21:42 +0530

    update

--- lectures/5-first-java-program/first-tutorial/Main.java
+++ lectures/5-first-java-program/first-tutorial/Main.java
@@ -1,5 +1,21 @@
-public class Demo {
+public class Main {
     public static void main(String[] args) {
-        System.out.println(args[0]);
+        System.out.println("Hello World");
+        System.out.println("Hello World".length());
+        // System.out.println(args);
+        System.out.println(args.length);
+        if (args.length >= 1) {
+            System.out.println(args[0]);
+        }
+
     }
 }
+
+// javac Main.java
+// java Main lalit
+// Hello World
+// 11
+// 1
+// lalit
+
+// javac -d "Path to store .class file(i.e compiled byte code)"
